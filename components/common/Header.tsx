c4a812901c7e9a3f98ccfb8b0330547c4a352a31
jyustrod <jyustrod@myuax.com>
2026-02-13 10:46:39 +0100

    Arreglos Menores

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import { getTranslation } from '@/lib/translations';
 import { navigationLinks } from '@/data/mockup';
 
